ef4d6da7dc93d752d29216d0c8798a702678fca3
Myst3ri0n <agilliard@gmail.com>
2015-11-05 19:39:29 -0500

    adding sample config file

---
+++ sample.config.py
@@ -0,0 +1,15 @@
+## Rename File to config.py when installing
+
+
+
+#==========================================================
+#Database config
+#==========================================================
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': 'localhost:8889',
+  'database': 'stocks',
+  'raise_on_warnings': True,
+}
+#==========================================================
