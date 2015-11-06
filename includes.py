d989747bcdcae3889070c095f979a423dde3da19
Myst3ri0n <agilliard@gmail.com>
2015-11-06 08:41:57 -0500

    Datetime Fix
    Changed date to the correct format for storing in the db.

--- includes.py
+++ includes.py
@@ -2,4 +2,4 @@ import time
 
 #converts epch date
 def convepoch(date):
-	return time.strftime('%m-%d-%Y %H:%M:%S',time.gmtime(int(date)/1000.))
+	return time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime(int(date)/1000.))
