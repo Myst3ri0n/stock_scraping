b773afab7ec01e220e477c1794acb46ed7740974
Myst3ri0n <agilliard@gmail.com>
2015-11-07 13:11:20 -0500

    Added initial support for basic finance info
    Still needs more work.

--- stock_get_db.py
+++ stock_get_db.py
@@ -1,4 +1,4 @@
-import urllib
+import urllib2
 import json
 import time
 import os
@@ -6,6 +6,7 @@ import includes
 import config
 import mysql.connector
 import re
+import requests
 
 #dbinfo
 conn=mysql.connector.connect(user='root',password='root',host='localhost',database='stocks')
@@ -19,7 +20,8 @@ print "This program scrapes and loads a list of stocks from the web to a databas
 print "what operation would you like to preform:"+'\n'
 print "1. Dowload stock prices to CSV."
 print "2. Import stock prices into the database."
-print "3. Import basic financials into the database."+'\n'
+print "3. Import basic financials into the database."
+print "4. Import List of Stocks from SEC site. (NYSE & NASDAQ)"+'\n'
 opSelect=raw_input('-->')
 
 
@@ -42,7 +44,7 @@ start_time = time.clock()
 #add stocks here to pull into csv
 symbols = ["AAPL","TSLA","GOOG","NFLX","AMD","GE","F","GM","CDW","AMZN","NVDA","ATVI","DG","MOS","CF",
 			"IFON","GE","BAC","FL","UBNT","RAI","MO","CRAY","FB","AKAM","CAT","INTC","IBM","DIS","COKE","CMG"]
-
+#symbols =["AAPL"]
 
 if extractType=='DB':	
 	print "Please wait..."+'\n'
@@ -50,7 +52,7 @@ if extractType=='DB':
 	count = 0
 	while i<len(symbols):
 		url = "http://www.bloomberg.com/markets/chart/data/"+timeFrame+"/"+symbols[i]+":US"
-		htmltext = urllib.urlopen(url)
+		htmltext = urllib2.urlopen(url)
 		data =json.load(htmltext)
 		datapoints = data["data_values"]
 	
@@ -78,7 +80,7 @@ elif extractType=='CSV':
 	count = 0
 	while i<len(symbols):
 		url = "http://www.bloomberg.com/markets/chart/data/"+timeFrame+"/"+symbols[i]+":US"
-		htmltext = urllib.urlopen(url)
+		htmltext = urllib2.urlopen(url)
 		data =json.load(htmltext)
 		datapoints = data["data_values"]
 	
@@ -98,6 +100,44 @@ elif extractType=='CSV':
 	print "Time taken... "+str(round(time.clock() - start_time,3)), "seconds"+'\n'
 	print fileName+".csv has been created @ "+os.getcwd()+'\n'
 else:
-	print "Under Construction: Try again Soon!"
+	print "Please wait..."+'\n'
+	i=0
+	count = 0
+	while i<len(symbols):
+		url = "https://www.google.com/finance?q="+symbols[i].lower()
+		htmltext = requests.get(url)
+		#print htmltext.text
+
+		regex = 'data-snapfield="(.*?)">'
+		pattern = re.compile(regex)
+		results = re.findall(pattern, htmltext.text)
+		for result in results:
+			#print result
+			regex = """<td class="key"
+          data-snapfield="""+'"'+result+'"'+""">(.*?)
+</td>
+<td class="val">(.*?)
+</td>"""
+			pattern = re.compile(regex)
+			results = re.findall(pattern, htmltext.text)
+			hySplit = re.split(' - ',str(results[0][1]))
+			#print result
+			#print hySplit[0]
+			if result == 'range':
+				query = "INSERT INTO stocks.basic_fin (time,symbol,day_range_low, day_range_high) VALUES('"+includes.nowDatetime()+"','"+symbols[i]+"','"+hySplit[0]+"','"+hySplit[1]+"');"
+				#print query
+				dbc.execute(query)
+				count +=1
+	
+
+			#dbc.execute("INSERT INTO stocks.ticks2 (symbol,time,price) VALUES ('"+symbols[i]+"','"+date+"','"+str(price)+"');")
+		i+=1
+	conn.commit()
+
+	print "Operation completed sucessfully:"+'\n'
+	print "Number of stocks scraped "+str(i)+'\n'
+	print "Number of lines imported "+str(count)+'\n'
+	print "Time taken... "+str(round(time.clock() - start_time,3)), "seconds"+'\n'
+
 	
 
