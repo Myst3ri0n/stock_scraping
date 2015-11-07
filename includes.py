fdab49fe5c8d862a3698692ba6b598d4bd2ba8b7
Myst3ri0n <agilliard@gmail.com>
2015-11-07 13:08:31 -0500

    Add function to return current date time

--- includes.py
+++ includes.py
@@ -3,3 +3,6 @@ import time
 #converts epch date
 def convepoch(date):
 	return time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime(int(date)/1000.))
+
+def nowDatetime():
+	return (time.strftime("%Y-%m-%d %H:%M:%S"))
